6754f62c057fc62f6befc949e35e92bc1f534f53
Adam Keys <adam@therealadam.com>
2012-01-01 16:35:12 -0600

    Import finger memory.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -9,3 +9,33 @@ alias ...='cd ../..'
 alias md='mkdir -p'
 alias rd=rmdir
 alias d='dirs -v'
+# vi
+alias vi="~/Applications/MacVim.app/Contents/MacOS/Vim"
+alias vim="~/Applications/MacVim.app/Contents/MacOS/Vim"
+
+# Editing
+alias e="vim"
+
+# Git
+alias g='git'
+
+# Network debugging
+alias traffic='sudo tcpdump -XS -i'
+alias httpflow='sudo tcpflow -c port 80'
+alias rc='restclient'
+
+# Gems
+alias gemi='gem install'
+alias gemun='gem uninstall'
+alias gems='gem search'
+
+# Servers
+alias start_mysql="mysql.server start"
+alias start_pgsql="pg_ctl -D /Users/adam/Develop/homebrew/var/postgres -l /Users/adam/Develop/homebrew/var/postgres/server.log start"
+alias start_memcached="memcached"
+alias start_redis="redis-server ~/Develop/homebrew/etc/redis.conf"
+alias start_mongo="mongod run --config /Users/adam/Develop/homebrew/Cellar/mongodb/1.6.1-x86_64/mongod.conf"
+alias start_cassandra="~/Develop/cassandra/bin/cassandra -f"
+
+# Stuff
+alias xmlpretty="xmllint --format -f"
--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -5,3 +5,20 @@ export GREP_COLOR='1;32'
 # pager
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
+
+# the greatest environment variable of all time
+export EDITOR="vi"
+
+# I put stuff in odd-ish places
+export PATH=/usr/local/bin:$PATH # My custom stuff
+export PATH=~/Develop/dotfiles/bin:$PATH # My scripts
+export PATH=/usr/local/mysql/bin:$PATH # MySQL
+export PATH=~/Develop/homebrew/bin:~/Develop/homebrew/sbin:$PATH # Homebrew
+export MANPATH=$MANPATH:/usr/local/man:/usr/local/mysql/man:/Users/adam/Develop/homebrew/share/man
+
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+
+export CDPATH=.:~/Develop/:~/Desktop/:/Volumes/Gowalla:/Volumes/NextUpdate
+
+# export ARCHFLAGS='-arch x86_64'
+
