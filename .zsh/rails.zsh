ad0663f796f85825e5bdbf478e683d49dfbf9804
Adam Keys <adam@therealadam.com>
2012-02-10 17:51:03 -0600

    Make Rails alias do the things I do.

--- .zsh/rails.zsh
+++ .zsh/rails.zsh
@@ -11,14 +11,13 @@ function _rails_command () {
 
 alias rc='_rails_command console'
 alias rd='_rails_command destroy'
-alias rdb='_rails_command dbconsole'
+alias rdbc='_rails_command dbconsole'
 alias rdbm='rake db:migrate db:test:clone'
 alias rg='_rails_command generate'
-alias rp='_rails_command plugin'
-alias ru='_rails_command runner'
+alias rr='_rails_command runner'
 alias rs='_rails_command server'
 alias rsd='_rails_command server --debugger'
 alias rlog='tail -f log/development.log'
-alias rdm='rake db:migrate'
-alias rdr='rake db:rollback'
+alias rdb='rake db:migrate'
+alias rdbr='rake db:rollback'
 
